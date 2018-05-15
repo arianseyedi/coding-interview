0015721dbd5ba619f2d25165226c310919e824ec
arianseyedi <arianseyedi@gmail.com>
2018-05-15 01:49:00 -0400

    rename project and class.

---
+++ coding-interview/src/algorithms_stringsArrays/stringsArrays.java
@@ -0,0 +1,13 @@
+package algorithms_stringsArrays;
+
+/**
+ * This class will contain a few methods that perform on strings for a certain
+ * purpose. They may seem a bit random, as they are, but will give insights as
+ * to how certain things can be done.
+ * 
+ * @author Arian
+ *
+ */
+public class stringsArrays {
+	
+}
